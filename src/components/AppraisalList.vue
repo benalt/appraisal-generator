b9d6b8a764f2aa5cc0f801e0bb2d0ea5f1d9da96
Ben Alt <ben.r.alt@gmail.com>
2023-10-05 08:52:53 -0500

    adding style to home page

--- src/components/AppraisalList.vue
+++ src/components/AppraisalList.vue
@@ -7,15 +7,27 @@ const appraisals = reactive( await Appraisal.loadAllAppraisals() )
 </script>
 
 <template>
-  <div>
+  <div class="main">
+    <h1>Available Appraisals</h1>
     <table>
-      <tr v-for="appraisal in appraisals">
+      <tr v-for="(appraisal, idx) in appraisals">
+        <td>{{ idx + 1 }}</td>
         <td><RouterLink :to="`/appraisal/${appraisal.id}`">{{ appraisal.title }}</RouterLink> </td>
         <td>{{ appraisal.valuationDate }}</td>
+        <td>{{appraisal.id}}</td>
       </tr>
     </table>
   </div>
 </template>
 
 <style scoped>
+.main {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 30px;
+}
+
+h1 {
+  border-bottom: 1px solid #cecece;
+}
 </style>
